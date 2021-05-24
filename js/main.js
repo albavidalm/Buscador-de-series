6592d30f7a695f170b85793b2a62de575d576996
Alba V <albavidalm@gmail.com>
2021-05-24 17:21:40 +0200

    Add favs

--- js/main.js
+++ js/main.js
@@ -23,6 +23,7 @@ function getApi() {
     .then((data) => {
       seriesData = data;
       createCard(seriesData);
+      createCardFavorite(favoritesData);
     });
 }
 
@@ -31,11 +32,17 @@ Como parámetro le paso (allInfo) que es cualquier nombre que le quiera dar en e
 function createCard(allInfo) {
   let htmlCardSerie = "";
   for (const serie of allInfo) {
-    htmlCardSerie += `<li class="serie__card js-serie-card" data-id= ${serie.show.id}>`;
-    if (serie.show.image === null) {
-      htmlCardSerie += `<img class="serie__card--img" src="${defaultImage}" alt="pic"/>`;
+    const isFav = false;
+    if (isFav) {
+      htmlCardSerie += `<li class="serie__card js-serie-card favorite" data-id= ${serie.show.id}>`;
     } else {
-      htmlCardSerie += `<img class="serie__card--img" src="${serie.show.image.medium}" alt="pic2" />`;
+      htmlCardSerie += `<li class="serie__card js-serie-card" data-id= ${serie.show.id}>`;
+    }
+
+    if (serie.show.image === null) {
+      htmlCardSerie += `<img class="serie__card--img" src="${defaultImage}" alt="Without cover" title="Without cover"/>`;
+    } else {
+      htmlCardSerie += `<img class="serie__card--img" src="${serie.show.image.medium}" alt="${serie.show.name} cover" title="${serie.show.name} cover" />`;
     }
     htmlCardSerie += `<h2 class="serie__card--title">${serie.show.name}</h2>`;
     htmlCardSerie += `</li>`;
@@ -44,7 +51,33 @@ function createCard(allInfo) {
   addListernerstoCard(); // la llamamos aquí xq es cuando creamos las fichas y existen
 }
 
+//PAINT FAVORITES
+function createCardFavorite(allFavs) {
+  let htmlCardSerie = "";
+  for (const serie of allFavs) {
+    htmlCardSerie += `<li class="serie__card favorite js-serie-card" data-id= ${serie.show.id}>`;
+    if (serie.show.image === null) {
+      htmlCardSerie += `<img class="serie__card--img" src="${defaultImage}" alt="Without cover" title="Without cover"/>`;
+    } else {
+      htmlCardSerie += `<img class="serie__card--img" src="${serie.show.image.medium}" alt="${serie.show.name} cover" title="${serie.show.name} cover" />`;
+    }
+    htmlCardSerie += `<h2 class="serie__card--title">${serie.show.name}</h2>`;
+    htmlCardSerie += `</li>`;
+  }
+  resultFavorites.innerHTML = htmlCardSerie; // Creo el html con el contenido superior
+  addListernerstoCard(); // la llamamos aquí xq es cuando creamos las fichas y existen
+}
+
+//____________________________________
+
 buttonSearch.addEventListener("click", getApi);
+
+//EVITAR QUE EL FORMULARIO SE ENVÍE VACÍO SI LA USUARIA LE DA A INTRO
+const form = document.querySelector(".js-form");
+function handleSubmit(event) {
+  event.preventDefault();
+  form.addEventListener("submit", handleSubmit);
+}
 
 /* FAVORITOS 
 A) Tenemos que saber que recoger los elementos sobre los que queremos actuar (les hemos puesto la misma clase) y los guardamos en una constante.
@@ -63,16 +96,29 @@ function addListernerstoCard() {
 function favoriteClick(event) {
   const cardLiClicked = event.currentTarget; //añadimos al elemento q escuchamos q es la tarjeta y con el current escucha al elemento madre donde ha sucedido el evento. Si quisiéramos exactamente donde hemos pinchado sería target
   // console.log("li", cardLiClicked); //Visualizar ID
-  cardLiClicked.classList.toggle("favorite");
-  const cardId = cardLiClicked.dataset.id;
+
+  const cardId = parseInt(cardLiClicked.dataset.id);
+  console.log("me han clickado", cardId);
   //console.log(cardId); Recoge OK la ID de la ficha
   // console.log("cardId", cardId); // ID recogido OK
-  const isFav = favoritesData.find((favoriteId) => favoriteId === cardId);
+  const isFav = favoritesData.find((favorite) => {
+    //   console.log(favorite, cardId);
+    return favorite.show.id === cardId;
+  });
+
   if (isFav === undefined) {
     //En esta indexación si esa ID clicada no está en mi array de favoritos súbemela
-    favoritesData.push(cardId);
+    const serieClicked = seriesData.find((serie) => {
+      console.log(serie.show.id, cardId);
+      return serie.show.id === cardId;
+    });
+    favoritesData.push(serieClicked);
   } else {
-    favoritesData = favoritesData.filter((favoriteId) => favoriteId !== cardId);
+    favoritesData = favoritesData.filter((serie) => serie.show.id !== cardId);
   }
   //console.log(favoritesData); //Nuevo favoritesData guardado OK & nueva búsqueda
+
+  createCard(seriesData);
+  createCardFavorite(favoritesData);
+  // guardar en ls
 }
